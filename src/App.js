ace4945aacfcb6e170e126b967cbefaaa6a0c5b7
Goody <101070150+Goody4444@users.noreply.github.com>
2023-03-14 00:00:39 +0100

    message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Sidebar from './Components/sidebar'
-import Login from './Components/login';
-import Main from './Components/main';
-import SpotifyMusicBox from './Components/SpotifyMusicBox'
-// import SignUp from './Components/signin';
+import React from 'react'
+import Home from './screens/home'
 
-function App() {
+export default function App() {
   return (
-    
-      <div className="App">
-         <Sidebar />
-         <Main />
-         <Login />
-         <SpotifyMusicBox />
-      </div>
-  );
-
-
-  }
-
-export default App;
+    <div>
+      <Home />
+    </div>
+  )
+}

ce4266e87c18cf5acb0253291cfd953b7f8005ef
Goody <101070150+Goody4444@users.noreply.github.com>
2023-03-13 18:03:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Sidebar from './Components/sidebar'
+import Login from './Components/login';
+import Main from './Components/main';
+import SpotifyMusicBox from './Components/SpotifyMusicBox'
+// import SignUp from './Components/signin';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="App">
+         <Sidebar />
+         <Main />
+         <Login />
+         <SpotifyMusicBox />
+      </div>
   );
-}
+
+
+  }
 
 export default App;
---
+++ src/Components/topbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+
+
+
+export default function Topbar(){
+   return(
+        <div class="topnav">
+           
+            <button type="button" className="split">Upgrade</button>
+            <br />
+        </div>
+    
+    )
+}
